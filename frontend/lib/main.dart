2a3f2a6f2c60e52749f5cb635db769447b3f4415
DanielWillforss <willforss66@gmail.com>
2026-06-28 14:18:18 +0200

    minor changes

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'My App',
+      title: 'Notes App',
       theme: ThemeData(
         // Main theme colors
         brightness: Brightness.dark,
